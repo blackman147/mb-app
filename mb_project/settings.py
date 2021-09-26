7afe291adf1da9bf4c17146ca391385c02a1b9a0
blackman147 <chuksntoka@gmail.com>
2021-09-26 14:42:48 +0100

    New updates for Heruko deployment

--- mb_project/settings.py
+++ mb_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-mp^dxxywjz2x2u-!0fk1t79g+*1zn&o03^vb*_*bz-auspy-0o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
